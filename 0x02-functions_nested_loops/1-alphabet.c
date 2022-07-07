bd6ecad7783ff851b56af185ec9dd8e8e81d7de1
enzoputachi <samuelnzoputachi@gmail.com>
2022-07-06 18:49:42 -0700

    Hot fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * print_alphabet - a function that prints the alphabet, in lowercase
  * Return: Always 0 (Succes)
  */
-int main(void)
+void print_alphabet(void)
 {
 	int n;
 
