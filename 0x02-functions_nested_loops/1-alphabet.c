9772b65d3418175845d577ebc7afd259a4ddbe7f
enzoputachi <samuelnzoputachi@gmail.com>
2022-07-06 19:01:43 -0700

    Hot f

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  * print_alphabet - a function that prints the alphabet, in lowercase
  * Return: Always 0 (Succes)
  */
-void print_alphabet(void)
+int main(void)
 {
 	int n;
 
